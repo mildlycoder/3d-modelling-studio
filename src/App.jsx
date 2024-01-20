5f4e835d725aeca9d1c039a6a1605793a3882901
Pranav Kumar <pranav.kumar@edufied.edu.vn>
2024-01-20 22:30:56 +0530

    added seo config

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import Home from './Home'
 import Contact from "./Contact"
 import Gallery from './Gallery'
 import Headers from './Components/Headers'
+import {Helmet} from "react-helmet";
 function App() {
 
   return (
     <>
+    <Helmet>
+        <title>DreamVizz</title>
+        <meta name="DreamVizz" content="DreamVizz is a Visualization firm for your needs"/>
+    </Helmet>
     <Headers />
     <Routes>
         <Route path="/" element={ <Home/> } />
