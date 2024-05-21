62e8543ed0122b6d9737959eec150c2801c530ea
Prathamesh Mudgale <prathameshmudgale@gmail.com>
2024-05-21 23:05:57 +0530

    changed logo and icon

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,7 +5,7 @@ import Footer from './Components/Footer'
 const Contact = () => {
   return (
     <div>
-      <section className="min-h-screen bg-cover relative" style={{ backgroundImage: "url('https://i.ibb.co/kcdL4vs/pexels-pixabay-271816.jpg')" }}>
+      <section className="min-h-screen bg-cover relative rounded-xl" style={{ backgroundImage: "url('https://i.ibb.co/kcdL4vs/pexels-pixabay-271816.jpg')" }}>
         <div className="absolute inset-0 bg-black/60"></div>
         <div className="flex flex-col min-h-screen text-white relative z-10">
           <div className="container flex flex-col flex-1 px-6 py-12 mx-auto">
